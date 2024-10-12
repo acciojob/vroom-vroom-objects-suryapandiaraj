b0f1775efde4229ffbf35ec9c1e0ff317185ac27
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2024-10-12 12:10:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make
+	this.model = model
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(make, model)
+	this.topSpeed = topSpeed
+}
+
+function getMakeModel(){
+	return `${make} ${model}`
+}
+
+function getTopSpeed(){
+	return `${topSpeed}`
+}
 
 // Do not change the code below
 window.Car = Car;
